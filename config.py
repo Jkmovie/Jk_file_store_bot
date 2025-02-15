2d3400de2313147c05c0c9715320f81f8b1145e5
Jkmovie <124867430+Jkmovie@users.noreply.github.com>
2025-02-15 22:05:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")  # Telegram Bot Token
 GDRIVE_FOLDER_ID = os.getenv("GDRIVE_FOLDER_ID")  # Google Drive Folder ID
 CREDENTIALS_FILE = "credentials.json"  # Google API Credentials
+MONGO_URI = os.getenv("MONGO_URI")  # MongoDB Connection String
