79daabbc362d00f2ecc157872ac4d7d01f730473
Jkmovie <124867430+Jkmovie@users.noreply.github.com>
2025-02-15 21:47:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-/FileStoreBot
-│── main.py
-│── 
-│── requirements.txt
-│── Procfile
-│── .env
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Telegram Bot Token
+GDRIVE_FOLDER_ID = os.getenv("GDRIVE_FOLDER_ID")  # Google Drive Folder ID
+CREDENTIALS_FILE = "credentials.json"  # Google API Credentials
