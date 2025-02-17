ebead524c1c59351e7eb0ce709273fd5b9f675ae
Jkmovie <124867430+Jkmovie@users.noreply.github.com>
2025-02-17 13:11:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 BOT_TOKEN = ""
 
 # MongoDB URI
-MONGO_URI = "mongodb://jkb73212:S74I9tDQxqJywRmJ@cluster0-shard-00-00.wwify.mongodb.net:27017,cluster0-shard-00-01.wwify.mongodb.net:27017,cluster0-shard-00-02.wwify.mongodb.net:27017/?replicaSet=atlas-xxxxxx-shard-0&ssl=true&authSource=admin"
+MONGO_URI = "mongodb+srv://jkb73212:S74I9tDQxqJywRmJ@cluster0.wwify.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Force Subscribe Channel
 FORCE_SUBSCRIBE_CHANNEL = "@jk_bot_area"
