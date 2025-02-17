dc138d01f82685a100f1b1a37499ee6bb22e11c4
Jkmovie <124867430+Jkmovie@users.noreply.github.com>
2025-02-17 12:41:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 BOT_TOKEN = "your_bot_token_here"
 
 # MongoDB URI
-MONGO_URI = "mongodb://jkb73212:Pj8Lqlut0CUi5M5j@<hostname>/?ssl=true&replicaSet=atlas-pgtvpp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://sampledhdj6:Y308LujZfMesIkpQ@cluster0.j9ha2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Force Subscribe Channel
 FORCE_SUBSCRIBE_CHANNEL = "@jk_bot_area"
